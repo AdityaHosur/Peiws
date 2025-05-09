1ce29dd84b494fd2babee2b9d80bbff2a1d0a430
AdityaHosur <158686277+AdityaHosur@users.noreply.github.com>
2025-05-09 22:49:11 +0530

    fix: react-router-dom issue

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders welcome message', () => {
+  render(
+      <App />
+  );
+  const welcomeElement = screen.getByText(/welcome to the peer review system/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
